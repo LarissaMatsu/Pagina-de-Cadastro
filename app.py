2666fe3411acab3ec3c8c030d0c52bd0c9957b3a
LarissaMatsu <108543821+LarissaMatsu@users.noreply.github.com>
2025-05-29 08:18:58 -0300

    l

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+USERS = {
+    "": ""
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', error=None)
+
+@app.route('/acesso', methods=['POST'])
+def acesso():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in USERS and USERS[username] == password:
+        return redirect('/servicos')
+    else:
+        logging.warning(f'Tentativa de login inválido: {username}')
+        return render_template('login.html', error="Usuário ou senha incorretos")
+
+@app.route('/servicos')
+def servicos():
+    return render_template('servicos.html')
+
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
