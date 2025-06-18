9178b47937c35fd5af27b3ea0b722fd28b9ce861
LarissaMatsu <108543821+LarissaMatsu@users.noreply.github.com>
2025-06-18 07:24:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,15 @@
-from flask import Flask, render_template, request, redirect
-import logging
+from flask import Flask
+from routes.main_routes import main_bp
+from routes.usuario_routes import usuario_bp
+from routes.carrinho_routes import carrinho_bp
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
+app.secret_key = 'yahoo'
 
-USERS = {
-    "": ""
-}
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html', error=None)
-
-@app.route('/acesso', methods=['POST'])
-def acesso():
-    username = request.form['username']
-    password = request.form['password']
-
-    if username in USERS and USERS[username] == password:
-        return redirect('/servicos')
-    else:
-        logging.warning(f'Tentativa de login inválido: {username}')
-        return render_template('login.html', error="Usuário ou senha incorretos")
-
-@app.route('/servicos')
-def servicos():
-    return render_template('servicos.html')
-
-
-@app.route('/cadastro', methods=['GET', 'POST'])
-def cadastro():
-    if request.method == 'POST':
-        email = request.form['email']
-        nome = request.form['nome']
-        senha = request.form['senha']
-        endereco = request.form['endereco']
-        datadenascimento = request.form['data de nascimento']
-        cpf = request.form['cpf']
-
-
-
-        return redirect(url_for('sucesso'))
-
-    return render_template('cadastro.html')
-
-@app.route('/sucesso')
-def sucesso():
-    return "Cadastro realizado com sucesso!"
+# Registrar os blueprints
+app.register_blueprint(main_bp)
+app.register_blueprint(usuario_bp, url_prefix='/usuario')
+app.register_blueprint(carrinho_bp, url_prefix='/carrinho')
 
 if __name__ == '__main__':
     app.run(debug=True)
